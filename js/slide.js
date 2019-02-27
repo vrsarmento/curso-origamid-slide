7e4237f178ed6506643e94cc6e3a6fc133dd2c59
Victor Rocha <vrsarmento@gmail.com>
2019-02-27 20:19:41 -0300

    Calculate distances on events

--- js/slide.js
+++ js/slide.js
@@ -2,6 +2,7 @@ export default class Slide {
 	constructor(slide, wrapper) {
 		this.slide = document.querySelector(slide);
 		this.wrapper = document.querySelector(wrapper);
+		this.distances = { finalPosition: 0, startX: 0, movement: 0 };
 	}
 
 	bindEvents() {
@@ -10,17 +11,30 @@ export default class Slide {
 		this.onEnd = this.onEnd.bind(this);
 	}
 
+	updatePosition(clientX) {
+		this.distances.movement = (this.distances.startX - clientX) * 1.6;
+		return this.distances.finalPosition - this.distances.movement;
+	}
+
+	moveSlide(distX) {
+		this.distances.movePosition = distX;
+		this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+	}
+
 	onStart(event) {
 		event.preventDefault();
 		this.wrapper.addEventListener('mousemove', this.onMove);
+		this.distances.startX = event.clientX;
 	}
 
 	onMove(event) {
-
+		const finalPosition = this.updatePosition(event.clientX);
+		this.moveSlide(finalPosition);
 	}
 
 	onEnd() {
 		this.wrapper.removeEventListener('mousemove', this.onMove);
+		this.distances.finalPosition = this.distances.movePosition;
 	}
 
 	addSlideEvents() {
