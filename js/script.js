92ea44dc197c1f011e786609f1271fc55520e2e8
Victor Rocha <vrsarmento@gmail.com>
2019-03-01 19:36:45 -0300

    Slide Nav Prev and Next

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-import Slide from './slide.js';
+import { SlideNav } from './slide.js';
 
-const slide = new Slide('.slide', '.slide-wrapper', 0);
+const slide = new SlideNav('.slide', '.slide-wrapper', 0);
 slide.init();
+slide.addArrow('.prev', '.next');
